95893c6f75711e39e9b1058e3eb0bca216ea151f
danielmunioz <Dan.munioz.el@gmail.com>
2022-03-24 21:20:15 +0000

    Adding Inference utils and minor changes

--- distilbert_reddit/datasets.py
+++ distilbert_reddit/datasets.py
@@ -40,14 +40,26 @@ class Sentiment140(Dataset):
             'label': self.df['label'].iloc[idx]}
 
 
+class inference_dataset(Dataset):
+  def __init__(self, dataframe):
+    self.df = dataframe
+  
+  def __len__(self):
+    return len(self.df)
+  
+  def __getitem__(self, idx):
+    return {'input_ids': self.df['input_ids'].iloc[idx],
+            'attention_mask': self.df['attention_mask'].iloc[idx]}
+
+
 class df_dataset(Dataset):
   def __init__(self, dataframe, label_name=None):
     """
-    Initializes a torch.dataset from a dataframe that contains the columns: 'input_ids', 'attention_mask', 'label'
+    Initializes a torch.dataset from a pandas dataframe containing: 'input_ids', 'attention_mask', 'label'
 
     Args:
       dataframe: A pandas dataframe
-      label_name: str, name of the column containing the labels, if none defaults to 'label'
+      label_name: str, name of the column containing the labels, defaults to 'label'
     """
     self.df = dataframe
     self.label_name = label_name
--- distilbert_reddit/testing.py
+++ distilbert_reddit/testing.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn.functional as F
 
+from utils import inference_dataset
+
 def inner_testing(model, data_loader, device, loss_fn=F.cross_entropy, keep_training=True):
   """
   Designed to test a model inside a training loop.
-  Recomended to use a loss from the torch.nn.funcional Module
+  Use of a functional loss (torch.nn.funcional) is recommended
 
-  Returns loss and accuracy as float values.
+  Returns global loss and accuracy.
   """
   loss = 0.0
   accuracy = 0.0
@@ -30,3 +32,36 @@ def inner_testing(model, data_loader, device, loss_fn=F.cross_entropy, keep_trai
     model.train()
     
   return loss, accuracy
+
+
+def inference(model, dframe, column, device, return_probs=True, batch_size=32):
+  tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+  collate_fn = Data_collator(tokenizer, using_labels=False)
+  mapper = tokenizer_map_df(tokenizer, column_name=column)
+  dframe = dframe.apply(mapper, axis=1)
+
+  dset = inference_dataset(dframe) #using inf_dset
+  dloader = DataLoader(dset, batch_size=batch_size, collate_fn=collate_fn)
+
+
+  soft_tensor = torch.Tensor([]).to(device)
+  preds_tensor = torch.Tensor([]).to(device)
+  model.eval()
+  with torch.no_grad():
+    for i, elemen in enumerate(dloader):
+      inputs, att_mask = elemen['input_ids'].to(device), elemen['attention_mask'].to(device)
+
+      out = model(inputs, att_mask)
+      soft_out = F.softmax(out, dim=1)
+      probs, preds = soft_out.topk(1, dim=1)
+
+      soft_tensor = torch.concat((soft_tensor, soft_out), dim=0)
+      preds_tensor = torch.concat((preds_tensor, preds), dim=0)
+
+
+  dframe = dframe.drop(columns=['input_ids', 'attention_mask'])
+  dframe['predictions'] = preds_tensor.cpu().numpy().astype(int)
+  if return_probs:
+    dframe = pd.concat([dframe.reset_index(), pd.DataFrame(soft_tensor.cpu().numpy())], axis=1)
+
+  return dframe
